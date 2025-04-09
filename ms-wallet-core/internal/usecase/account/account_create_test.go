974c2d9005e4de747bac47a747b99c2f66ddfbbd
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-04-09 15:46:08 -0300

    [Account]: standardization

--- ms-wallet-core/internal/usecase/account/account_create_test.go
+++ ms-wallet-core/internal/usecase/account/account_create_test.go
@@ -4,44 +4,17 @@ import (
 	"testing"
 
 	"github.com/silverioTenor/fc-wallet-system/internal/entity"
+	fake "github.com/silverioTenor/fc-wallet-system/internal/gateway/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ClientGatewayMock struct {
-	mock.Mock
-}
-
-func (m *ClientGatewayMock) Save(client *entity.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Client), args.Error(1)
-}
-
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (m *AccountGatewayMock) Save(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
-func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
 func TestCreateAccountUseCase_Execute(t *testing.T) {
 	client, _ := entity.NewClient("Willy Wonka", "willy@wonka.com")
-	clientMock := &ClientGatewayMock{}
+	clientMock := &fake.ClientGatewayMock{}
 	clientMock.On("Get", client.ID).Return(client, nil)
 
-	accountMock := &AccountGatewayMock{}
+	accountMock := &fake.AccountGatewayMock{}
 	accountMock.On("Save", mock.Anything).Return(nil)
 
 	usecase := NewCreateAccountUseCase(accountMock, clientMock)
